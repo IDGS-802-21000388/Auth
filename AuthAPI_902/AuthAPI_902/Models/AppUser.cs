ee1d3c4128909af882557152783e3f58c623cf7a
Jorge Morales <21000388@alumnos.utleon.edu.mx>
2024-08-05 22:32:58 -0600

    Auth App

---
+++ AuthAPI_902/AuthAPI_902/Models/AppUser.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace AuthAPI_902.Models
+{
+    public class AppUser : IdentityUser
+    {
+        public string? FullName { get; set; } // para agregar mas datos al modelo de IdentityUser
+        public string? RefreshToken { get; set; }
+        public DateTime RefreshTokenExpiryTime { get; set; }
+    }
+}
